933872ed97c90d095172b1b9b2bb344cb75cd1eb
Zoez0802 <minjiez@bu.edu>
2025-10-29 18:26:19 -0400

    imrpoving

--- getData.ts
+++ getData.ts
@@ -3,7 +3,7 @@ const API_KEY = process.env.EXCHANGE_API_KEY;
 export async function getData(base: string = "USD") {
     const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${base}`;
 
-    const res = await fetch(url, { cache: "no-store" });
+    const res = await fetch(url);
 
     if (!res.ok) {
         throw new Error(`API call failed :${res.status} ${res.statusText}`);
